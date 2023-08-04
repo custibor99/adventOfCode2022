f9e92af09f743db48bad5d92df605101c34b231a
custibor99 <tcus@outbrain.com>
2023-08-04 14:26:11 +0200

    add day 2 second part solution

--- day2/main.go
+++ day2/main.go
@@ -40,18 +40,45 @@ func scoreRound(playerShape int, oponentShape int) int {
 
 
 func parseInput(oponent string, player string) (int, int, error) {	
-	playerShape, exists :=shapeMap[player]
+	playerShape, exists := shapeMap[player]
 	if (exists == false) {
 		return 0, 0, errors.New("Invalid player input")
 	}
 
-	oponentShape, exists :=shapeMap[oponent]
+	oponentShape, exists := shapeMap[oponent]
 	if (exists == false) {
 		return 0, 0,  errors.New("Invalid oponent input")
 	}
 	return playerShape, oponentShape, nil
 }
 
+func parseInput2(oponent string, player string) (int, int, error) {	
+	oponentShape, exists := shapeMap[oponent]
+	if (exists == false) {
+		return 0, 0,  errors.New("Invalid oponent input")
+	}
+
+	_, exists = shapeMap[player]
+	if (exists == false) {
+		return 0, 0, errors.New("Invalid player input")
+	}
+
+	if player == "Y" {
+		return oponentShape, oponentShape, nil
+	} else if player == "Z" {
+		playerShape := (oponentShape + 1) % 3
+		if playerShape == 0 {
+			playerShape = 3
+		}
+		return playerShape, oponentShape, nil
+	} else {
+		playerShape := oponentShape - 1
+		if playerShape == 0{
+			playerShape = 3
+		}
+		return playerShape, oponentShape, nil
+	}
+}
 
 func main() {
 	var filename string = os.Args[1]
@@ -64,6 +91,7 @@ func main() {
 	
 	scanner := bufio.NewScanner(file)
 	totalScore := 0
+	totalScore2 := 0
 	for scanner.Scan() {
 		line := scanner.Text()
 		elements := strings.Split(line, " ")
@@ -76,8 +104,16 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+
+		playerShape2, oponentShape2, err := parseInput2(elements[0], elements[1])
+		if err != nil {
+			log.Fatal(err)
+		}
 		roundScore := scoreRound(playerShape, oponentShape)
+		roundScore2 := scoreRound(playerShape2, oponentShape2)
 		totalScore += roundScore 
+		totalScore2 += roundScore2
 	}
 	fmt.Println(totalScore)
+	fmt.Println(totalScore2)
 }
