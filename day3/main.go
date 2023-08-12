310d5d0c91dfa09bb2dad109bb7ffc05298d2608
custibor99 <tcus@outbrain.com>
2023-08-12 21:30:44 +0200

    day 3 final solution

--- day3/main.go
+++ day3/main.go
@@ -19,7 +19,8 @@ func main() {
 	}
 
 	defer f.Close()
-
+	scanner := bufio.NewScanner(f)
+	/*
 	scanner := bufio.NewScanner(f)
 	score := 0
 	for scanner.Scan() {
@@ -30,6 +31,22 @@ func main() {
 		s2 := list[half:]
 		duplicates := utils.FindDuplicateElements(s1, s2)
 		score += utils.ScoreElements(duplicates)		
+	}
+	fmt.Println(score)
+	*/
+
+	var backpacks [3][]int
+	i := 0
+	score := 0
+	for scanner.Scan() {
+		input := scanner.Text()
+		backpacks[i % 3] = utils.StrToIntList(input)
+		if i % 3 == 2 {
+			duplicates := utils.FindDuplicateElements(backpacks[0], backpacks[1])
+			duplicates = utils.FindDuplicateElements(duplicates, backpacks[2])
+			score += utils.ScoreElements(duplicates)
+		}
+		i++
 	}
 	fmt.Println(score)
 }
